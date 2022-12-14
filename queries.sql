286a7eefdbcd26ad40a56851f123aa5052ba836c
Olivier-Kango <olivierkango@gmail.com>
2022-12-13 20:26:49 -0800

    Add some correctionsin queries.sql

--- queries.sql
+++ queries.sql
@@ -1,10 +1,10 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM  animals WHERE date_of_birth BETWEEN '2015-12-31' AND '2020-01-01';
+SELECT name FROM  animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name FROM animals WHERE neutered AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.39 AND 17.31;
+SELECT * FROM animals WHERE name NOT LIKE 'Gabumon';
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
