4031f434b56996aa3dcc2518bb268179a8ee09dc
john <kiokoj81@gmail.com>
2022-12-21 12:49:10 +0300

    Join tables from many-to-many relationships

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -50,3 +50,26 @@ CREATE TABLE invoices_items (
   FOREIGN KEY (treatment_id)
   REFERENCES treatements(id)
 );
+
+-- Create (join-table between medical_histories and treatments) medical_treatments table.
+CREATE TABLE medicals_treatments(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  treatment_id INT,
+  medical_history_id INT,
+  PRIMARY KEY (id),
+  CONSTRAINT fk_treatments
+    FOREIGN KEY (treatment_id)
+      REFERENCES treatments(id),
+  CONSTRAINT fk_medical_histories
+    FOREIGN KEY (medical_history_id)
+      REFERENCES medical_histories(id)
+);
+
+-- FK Indexes
+CREATE INDEX medical_histories_patient_id ON medical_histories(patient_id);
+CREATE INDEX invoices_medical_history_id ON invoices(medical_history_id);
+CREATE INDEX invoices_items_invoiced_id ON invoices(invoiced_id);
+CREATE INDEX invoices_items_treatment_id ON treatments(treatment_id);
+CREATE INDEX medicals_treatments_treatment_id ON treatments(treatment_id);
+CREATE INDEX medicals_treatments_medical_history_id ON medical_histories(medical_history_id);
+
