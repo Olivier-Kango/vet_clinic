7283dfc199bd06680f18678c80b67d33837c41fa
Olivier-Kango <olivierkango@gmail.com>
2022-12-20 16:55:39 -0800

    Decrease the execution time

--- queries.sql
+++ queries.sql
@@ -233,3 +233,9 @@ ON species.id = animals.species_id
 WHERE vets.name = 'Maisy Smith'
 GROUP BY species.name
 ORDER BY count DESC LIMIT 1;
+
+-- Find a way to decrease the execution time of the first query. Look for hints in the previous lessons.
+SELECT COUNT (*) FROM visits WHERE animal_id = 1;
+
+CREATE INDEX visits_animal_id
+ON visits(animal_id);
