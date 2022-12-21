34d53fc23aea5fbfb002c6a5f17b213dde26afa7
Olivier-Kango <olivierkango@gmail.com>
2022-12-20 18:37:02 -0800

    Add explain analyze

--- queries.sql
+++ queries.sql
@@ -238,14 +238,17 @@ ORDER BY count DESC LIMIT 1;
 SELECT COUNT (*) FROM visits WHERE animal_id = 4;
 CREATE INDEX visits_animal_id
 ON visits(animal_id);
+EXPLAIN ANALYZE SELECT COUNT (*) FROM visits WHERE animal_id = 4;
 
 
 -- decrease the execution time
 SELECT * FROM visits where vet_id = 2;
 CREATE INDEX visits_vet_id ON visits(vet_id);
+EXPLAIN ANALYZE SELECT * FROM visits WHERE animal_id = 2;
 
 -- decrease the execution time
 SELECT * FROM owners where email = 'owner_18327@mail.com';
 CREATE INDEX owners_email ON owners(email);
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
